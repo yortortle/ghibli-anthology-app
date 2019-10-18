eef1467e8ede4d7776e8ab0f01f177de638789c4
Troy Hancock <Troy@Troys-MacBook-Pro.local>
2019-10-18 16:09:16 -0400

    working on sorting array of objects

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 
 
 $ ( () => {
-  const $body = $("body");
   let input = "";
+  const directorList = [];
+
+  const $body = $("body");
   const $titleBtn = $("#title");
   const $directorBtn = $("#director");
   const $locationBtn = $("#location");
+  const $list = $(".list");
+  const $center = $(".center")
+  const $generatedBtns = $("#generatedBtns");
 
   const promise = $.ajax({
     url: "https://ghibliapi.herokuapp.com/films/"
@@ -17,10 +22,29 @@ $ ( () => {
         if (input === "TITLE") {
           console.log(data);
           for (var i = 1; i <= data.length; i++) {
-            console.log(i + ": " + data[i].title + " " + data[i].director);
-            // console.log(data[i].director);
+            // console.log(i + ": " + data[i].title + " - Director: " + data[i].director + " - Release Date: " + data[i].release_date);
+            const $output = $("<p>");
+            const $buttonOutput = $("<button>").text("MORE INFO").addClass("buttons").attr("id", "generatedBtns");
+            $output.text(i + ": " + data[i].title + " - Director: " + data[i].director + " - Release Date: " + data[i].release_date);
+            $output.append($buttonOutput);
+            $list.append($output);
           }
         }
+        else if (input === "DIRECTOR") {
+          $list.empty();
+          console.log("success");
+          for (var i = 1; i < data.length; i++) {
+            console.log(data[i].director);
+            directorList.push(data[i].director);
+          }
+          console.log(directorList);
+          directorList.sort();
+          console.log(directorList);
+          for (var i = 0; i < data.length; i++) {
+            
+          }
+        }
+
       },
       ()=>{
         console.log("oops");
@@ -29,8 +53,14 @@ $ ( () => {
   }
 
   $titleBtn.on("click", () => {
-    input = "TITLE"
+    input = "TITLE";
     console.log(input);
+    func();
+  })
+
+  $directorBtn.on("click", () => {
+    input = "DIRECTOR";
+    console.log("test");
     func();
   })
 
